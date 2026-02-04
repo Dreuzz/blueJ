1949f2c94504b8bed4b9c422e92c4c4aaf8ca136
Dreuzz <dreuzzz@proton.me>
2026-02-04 13:13:44 +0100

    HA anfang

---
+++ schidsrichter/Spieler.java
@@ -0,0 +1,21 @@
+
+/**
+ * @author 
+ * @version 
+ */
+public class Spieler
+{
+    private String name;
+    private int nummer;
+    private int anzGelb;
+    private boolean imSpiel;
+    
+    public Spieler(String pName, int pNummer, int pAnzGelb, boolean pImSpiel)
+    {
+        name = pName;
+        nummer = pNummer;
+    }
+
+    // Dienste
+
+}
